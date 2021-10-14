9f6964f25cb3c705bafb3261eb70cf72bdf9c4c0
Sungeheon <elon4856@gmail.com>
2021-10-15 00:24:27 +0900

    feat: get post기능을 추가하세요.

--- views/main_views.py
+++ views/main_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
+from model.models import Post
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -6,3 +7,13 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('/')
 def hello_world():
     return 'Hello World!22'
+
+@bp.route('/posts')
+def posts():
+    post_list = Post.query.order_by(Post.create_date.desc())
+    return render_template('post/post_list.html', post_list=post_list)
+
+@bp.route('/posts/<int:post_id>/')
+def detail(post_id):
+    post = Post.query.get(post_id)
+    return render_template('post/post_detail.html', post=post)
