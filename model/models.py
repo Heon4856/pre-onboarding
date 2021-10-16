01ab539bb9dbe5a05d31c109a673ca11fd010cb0
Sungeheon <elon4856@gmail.com>
2021-10-16 20:26:52 +0900

    feat: id에 autocrement를 추가하세요.

--- model/models.py
+++ model/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
