b7c9814095be57d9d04207a0980bebf85c5db812
Sungeheon <elon4856@gmail.com>
2021-10-17 23:14:33 +0900

    feat: 회원가입 기능을 추가하세요.

--- model/models.py
+++ model/models.py
@@ -7,3 +7,8 @@ class Post(db.Model):
     create_date = db.Column(db.DateTime(), nullable=False)
     modify_date = db.Column(db.DateTime(), nullable=True)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
