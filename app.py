9b2e83e08d1da712ba3a309a40ae270504e86d9c
Sungeheon <elon4856@gmail.com>
2021-10-18 02:36:07 +0900

    add: blueprint를 추가하세요.

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ def create_app():
     from model import models
 
     # 블루프린트
-    from views import main_views
-    app.register_blueprint(main_views.bp)
+    from views import post_views, auth_views
+    app.register_blueprint(post_views.bp)
+    app.register_blueprint(auth_views.bp)
 
     return app
 
