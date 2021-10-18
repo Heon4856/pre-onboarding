b4606cedec711d768ae29e4df9fa2e2166a55f65
Sungeheon <elon4856@gmail.com>
2021-10-18 14:55:37 +0900

    feat: 404에러 메세지 보내는 기능을 구현하세요.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
@@ -25,6 +25,9 @@ def create_app():
     #jwt
     jwt = JWTManager(app)
 
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return jsonify(msg="없는 페이지 입니다. 아마 존재하지 않는 게시글 id로 요청을 보내셨을 가능성이 높습니다.", status_code=404)
     return app
 
 
