7e053e99d8a87e8774640e2a0a79659c03f2ba75
Sungeheon <elon4856@gmail.com>
2021-10-18 11:04:43 +0900

    fix: post serializer를 적용하세요.

---
+++ serializers/post.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields
+from model.models import Post
+
+
+class PostSchema(Schema):
+    class Meta:
+        model = Post
+
+    id = fields.Integer()
+    subject = fields.String(allow_none=False)
+    content = fields.String(allow_none=False)
+    create_date = fields.DateTime(allow_none=False)
+    modify_date = fields.DateTime(allow_none=True)
+
+
+class PostListSchema(Schema):
+    post_list = fields.List(fields.Nested(PostSchema))
