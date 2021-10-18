9bb15348f299ff83f85225690ca18162387ab78b
Sungeheon <elon4856@gmail.com>
2021-10-18 15:32:40 +0900

    feat: secret key추가하세요.

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'bulletin.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SECRET_KEY = "dev"
+JWT_SECRET_KEY = "super-secret"
