69538563f58b833703a1c7f3ff4507de793e6298
Sungeheon <elon4856@gmail.com>
2021-10-21 01:53:30 +0900

    refactor: 잘못된 로그인 시도에 대한 경우에 대비하는 코드를 작성하세요.

--- views/auth_views.py
+++ views/auth_views.py
@@ -19,4 +19,8 @@ def signup(username, password):
 @use_kwargs(AuthRequestSchema)
 @marshal_with(TokenSchema, code=200)
 def login(username, password):
-    return jsonify(access_token=auth_service.login(username, password), status_code=200)
+    is_login_success = auth_service.login(username, password)
+    if is_login_success:
+        return jsonify(access_token=is_login_success, status_code=200)
+    else:
+        return  make_response(jsonify(msg='잘못된 로그인 정보입니다.', status_code=404), 404)
