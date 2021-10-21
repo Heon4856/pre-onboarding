190a3ba667180bb734bf1560450de64503dcb618
Sungeheon <elon4856@gmail.com>
2021-10-21 10:23:28 +0900

    refactor:비밀번호 암호화기능에 알맞게 테스트코드를 수정하고, 주석을 추가하세요.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -23,13 +23,13 @@ class TestCase(unittest.TestCase):
         db.drop_all()
 
     def test_signup(self):
-        """회원가입 테스트"""
+        """회원가입을 하면 201상태코드를 반환한다."""
         test_data = {"username": "test1100", "password": "test"}
         rv = self.client.post('/auth/signup/', data=json.dumps(test_data), content_type='application/json')
         assert 201 == rv.status_code
 
     def test_login_api_with_valid_login_information(self):
-        """로그인 테스트"""
+        """유효한 로그인 정보로 로그인 시도시 200상태코드와 access_token을 반환"""
         test_data = {"username": "test1", "password": "test1"}
         rv = self.client.post('/auth/login/', data=json.dumps(test_data), content_type='application/json')
         print(rv.status_code)
@@ -37,13 +37,13 @@ class TestCase(unittest.TestCase):
         assert b'access_token' in rv.data
 
     def test_login_with_wrong_password(self):
-        """잘못된 비밀번호로 로그인 시도"""
+        """잘못된 비밀번호로 로그인 시도시 404 상태코드를 반환한다."""
         test_data = {"username": "test1", "password": "wrong_password"}
         rv = self.client.post('/auth/login/', data=json.dumps(test_data), content_type='application/json')
         assert 404 == rv.status_code
 
     def test_login_with_not_found_username(self):
-        """찾을 수 없는 유저이름으로 로그인 시도"""
+        """찾을 수 없는 유저이름으로 로그인 시도시 404 상태코드를 반환한다."""
         test_data = {"username": "wron_id", "password": "test122"}
         rv = self.client.post('/auth/login/', data=json.dumps(test_data), content_type='application/json')
         assert 404 == rv.status_code
